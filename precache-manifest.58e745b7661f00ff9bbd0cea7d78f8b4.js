f55ab2c19476c18e5d92d2664e149262bdabb2a8
bhishp <bhishma_p@hotmail.com>
2018-11-26 20:55:22 +0000

    Updates

---
+++ precache-manifest.58e745b7661f00ff9bbd0cea7d78f8b4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "22c0647fea0d809484f8",
+    "url": "/bhishp-cv/static/css/main.57f255bf.chunk.css"
+  },
+  {
+    "revision": "22c0647fea0d809484f8",
+    "url": "/bhishp-cv/static/js/main.22c0647f.chunk.js"
+  },
+  {
+    "revision": "c20b5e1e3c12f4085e86",
+    "url": "/bhishp-cv/static/js/1.c20b5e1e.chunk.js"
+  },
+  {
+    "revision": "4b929f44b91bf3da1d65",
+    "url": "/bhishp-cv/static/js/runtime~main.4b929f44.js"
+  },
+  {
+    "revision": "053be053d9931657f17dfac875266795",
+    "url": "/bhishp-cv/static/media/react-icon.053be053.png"
+  },
+  {
+    "revision": "fe60c1162156d77f797824e55781ef0f",
+    "url": "/bhishp-cv/static/media/meteor-icon.fe60c116.png"
+  },
+  {
+    "revision": "cc4efea1f69a3244db552eda1d4b7966",
+    "url": "/bhishp-cv/static/media/node-icon.cc4efea1.png"
+  },
+  {
+    "revision": "b1f8dc0225869cf05694e6782a9d8db5",
+    "url": "/bhishp-cv/static/media/redux-icon.b1f8dc02.png"
+  },
+  {
+    "revision": "7f6d04e242ca864bf1cddfa30bc1cf41",
+    "url": "/bhishp-cv/static/media/64px-docker.7f6d04e2.png"
+  },
+  {
+    "revision": "7073d1f15d3a03eeb4f2008c31c1fa86",
+    "url": "/bhishp-cv/static/media/64px-aws.7073d1f1.png"
+  },
+  {
+    "revision": "fa2650ae36ea3b6b9ad73e592a5035de",
+    "url": "/bhishp-cv/static/media/csharp-icon.fa2650ae.png"
+  },
+  {
+    "revision": "feb6f8858516a29a4f8b6e562dcf79af",
+    "url": "/bhishp-cv/static/media/spring.feb6f885.png"
+  },
+  {
+    "revision": "9d2f5f920b6e9dcd024378de85f19998",
+    "url": "/bhishp-cv/static/media/sql-icon.9d2f5f92.png"
+  },
+  {
+    "revision": "61c6706b801844a25663ea535551dc5a",
+    "url": "/bhishp-cv/static/media/java-logo.61c6706b.png"
+  },
+  {
+    "revision": "9d62535aa28273af8d83a0005c2e7a27",
+    "url": "/bhishp-cv/static/media/haskell.9d62535a.png"
+  },
+  {
+    "revision": "51092ddc837f4cfd95a16e99da8b4010",
+    "url": "/bhishp-cv/static/media/bhish-superman.51092ddc.png"
+  },
+  {
+    "revision": "7d3d597a3480a8edcd4df490e15ed657",
+    "url": "/bhishp-cv/static/media/BEBAS.7d3d597a.ttf"
+  },
+  {
+    "revision": "674479c1de58f11ecd3e337ea975d0a6",
+    "url": "/bhishp-cv/index.html"
+  }
+];
